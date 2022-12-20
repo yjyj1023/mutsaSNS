d04ca626729b16e31e6e206fb2f5e12e2c04b82b
yjyj1023 <lyj19961023@gmail.com>
2022-12-20 15:28:01 +0900

    HelloController 생성

---
+++ src/main/java/com/mutsasns/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.mutsasns.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok().body("hello");
+    }
+}
