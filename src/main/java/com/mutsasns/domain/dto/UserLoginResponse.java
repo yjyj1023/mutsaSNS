15ef69febd5893b4a9048ed6b64332b65dd9a0be
yjyj1023 <lyj19961023@gmail.com>
2022-12-21 14:51:06 +0900

    로그인(Login) 기능 개발 1. userName 맞는지 체크 2. password 맞는지 체크 3. 1번,2번 통과시 로그인 성공 후 토큰 리턴

---
+++ src/main/java/com/mutsasns/domain/dto/UserLoginResponse.java
@@ -0,0 +1,12 @@
+package com.mutsasns.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserLoginResponse {
+    private String token;
+}
