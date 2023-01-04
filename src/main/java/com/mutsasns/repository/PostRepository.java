515837ac211ec99fefc7f8fdaab841ac9a4366b2
yjyj1023 <lyj19961023@gmail.com>
2023-01-04 11:50:50 +0900

    마이피드 기능 추가

--- src/main/java/com/mutsasns/repository/PostRepository.java
+++ src/main/java/com/mutsasns/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.mutsasns.repository;
 
 import com.mutsasns.domain.post.Post;
+import com.mutsasns.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAllByUser(User user, Pageable pageable);
 }
