3a006f8a4c91b702262d8ce06feb84aa909d62a2
yjyj1023 <lyj19961023@gmail.com>
2023-01-05 20:30:40 +0900

    comment 기능 추가

---
+++ src/main/java/com/mutsasns/domain/comment/Comment.java
@@ -0,0 +1,42 @@
+package com.mutsasns.domain.comment;
+
+import com.mutsasns.domain.Base;
+import com.mutsasns.domain.comment.dto.CommentResponse;
+import com.mutsasns.domain.post.Post;
+import com.mutsasns.domain.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Comment extends Base {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public CommentResponse toResponse() {
+        return CommentResponse.builder()
+                .id(this.id)
+                .comment(this.comment)
+                .userName(this.user.getUserName())
+                .postId(this.post.getId())
+                .createdAt(this.getCreatedAt())
+                .lastModifiedAt(this.getLastModifiedAt())
+                .build();
+    }
+}
